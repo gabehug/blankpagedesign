9cee94d19a95732ed98fab869cba0012383fccb5
gabehug <ghug@luc.edu>
2022-08-01 09:09:07 -0500

    added new cursor

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('name present', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Gabe Hug/i);
   expect(linkElement).toBeInTheDocument();
 });
